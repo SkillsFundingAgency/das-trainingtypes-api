f101f4d84833dac41a9165245a286af01ea67113
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2025-06-13 10:40:10 +0100

    change shortCode

--- src/SFA.DAS.CourseTypes.Domain/CourseTypes/FoundationApprenticeship.cs
+++ src/SFA.DAS.CourseTypes.Domain/CourseTypes/FoundationApprenticeship.cs
@@ -4,7 +4,7 @@ namespace SFA.DAS.CourseTypes.Domain.CourseTypes;
 
 public class FoundationApprenticeship : CourseType
 {
-    public override string ShortCode => "Foundation";
+    public override string ShortCode => "FoundationApprenticeship";
     public override RecognitionOfPriorLearning RecognitionOfPriorLearning => new RecognitionOfPriorLearningNotRequired();
     public override LearnerAge LearnerAge => new LearnerAge(minimumAge: 16, maximumAge: 25);
     public override CourseDuration CourseDuration => new CourseDuration(minimumDurationMonths: 8, maximumDurationMonths: 48);
