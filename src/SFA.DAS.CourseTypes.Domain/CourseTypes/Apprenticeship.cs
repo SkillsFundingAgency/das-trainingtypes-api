2f8c364632ab9186d6298ae54ad8f017e21ece8f
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2025-06-17 14:17:17 +0100

    update otj training hours

--- src/SFA.DAS.CourseTypes.Domain/CourseTypes/Apprenticeship.cs
+++ src/SFA.DAS.CourseTypes.Domain/CourseTypes/Apprenticeship.cs
@@ -5,7 +5,7 @@ namespace SFA.DAS.CourseTypes.Domain.CourseTypes;
 public class Apprenticeship : CourseType
 {
     public override string ShortCode => "Apprenticeship";
-    public override RecognitionOfPriorLearning RecognitionOfPriorLearning => new RecognitionOfPriorLearningRequired(offTheJobTrainingMinimumHours: 200);
-    public override LearnerAge LearnerAge => new LearnerAge(minimumAge: 16, maximumAge: 67);
-    public override CourseDuration CourseDuration => new CourseDuration(minimumDurationMonths: 8, maximumDurationMonths: 48);
+    public override RecognitionOfPriorLearning RecognitionOfPriorLearning => new RecognitionOfPriorLearningRequired(offTheJobTrainingMinimumHours: 187);
+    public override LearnerAge LearnerAge => new(minimumAge: 16, maximumAge: 67);
+    public override CourseDuration CourseDuration => new(minimumDurationMonths: 8, maximumDurationMonths: 48);
 }
